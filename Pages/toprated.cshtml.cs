76b0a40637dd21cf1663acbfd505db50262b8ba5
Elliot Burns <eb05263952@priestley.ac.uk>
2025-01-28 14:49:38 +0000

    created a top rated and suggested page

---
+++ Pages/toprated.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace appPrototype_FFC.Pages
+{
+    public class topratedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
